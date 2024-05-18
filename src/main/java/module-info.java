7d091bf8e3cd6451ece5529762532147dd511c8f
inanyan <ruslanpopov1512@gmail.com>
2024-05-18 12:03:49 +0300

    Make it simpler

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
 module org.example {
-    requires langchain4j;
+    requires langchain4j.core;
+    requires langchain4j.embeddings.all.minilm.l6.v2;
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,37 +2,11 @@ package org.example;
 
 import java.util.Scanner;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
+import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.err.println("Error: provide an access token");
-            System.exit(1);
-        } else if (args.length > 1) {
-            System.err.println("Error: too many arguments");
-            System.exit(1);
-        }
-
-        ChatLanguageModel chatLanguageModel = HuggingFaceChatModel
-                .builder()
-                .modelId("microsoft/Phi-3-mini-128k-instruct")
-                .accessToken(args[0])
-                .build();
-
-        Scanner scanner = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("User: ");
-            String userPrompt = scanner.nextLine();
-
-            if (userPrompt.equals("exit")) {
-                break;
-            }
-
-            System.out.print("AI: ");
-            System.out.println(chatLanguageModel.generate(userPrompt));
-        }
+        EmbeddingModel model = new AllMiniLmL6V2EmbeddingModel();
     }
 }
